883ef447dd65dbe18f4d0c02dc0c2c3e1d6ed1ee
Suyog3g <113542889+Suyog3g@users.noreply.github.com>
2023-04-12 15:19:26 -0400

    updated html,css,js
    updated html,css, js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const navIcon = document.querySelector('#nav-icon1');
+const burgerCon = document.querySelector('#burger-con');
+
+navIcon.addEventListener('click', function() {
+  navIcon.classList.toggle('open');
+  burgerCon.classList.toggle('show');
+});
+
+
+
+
+
+const submitBtn = document.querySelector('#submit input[type="submit"]');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.getElementById('close-btn');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault(); // prevent the form from submitting
+  overlay.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  overlay.style.display = 'none';
+});
+
+window.addEventListener('load', () => {
+  overlay.style.display = 'none';
+});
